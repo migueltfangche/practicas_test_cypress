d54416d3f17b26a4e3868378058226d4ea375d8b
MiguelTfangche <migueltfangche@gmail.com>
2024-11-26 11:41:52 -0300

    Primer mensaje

---
+++ cypress/e2e/test-fixture.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+
+describe('Test con Studio Cypress', () => {
+  var mydata // 1 . creacion de la variable global
+  beforeEach('Conexion a Instituto Web login', () => {
+    cy.visit('https://www.institutoweb.com.ar/test/login.html')
+    cy.fixture('./datos_test') // 2 . conectar con el archivo
+    .then(test =>{  // 3 . cuando llegan los datos
+      mydata = test // 4 . lo que llega lo paso a mydata
+    })
+  })
+
+
+  it('passes', () => {
+    mydata.forEach((data) => { // lee renglon a reglon de los 10 datos del json
+      cy.visit('https://www.institutoweb.com.ar/test/login.html')
+      /* ==== Generated with Cypress Studio ==== */
+      cy.get('#tuusuario').type(data.usuario);
+      cy.get('#tuclave').type(data.clave);
+      cy.get('#tumail').type(data.email);
+      cy.get(':nth-child(8)').click();
+      cy.get('h3').should('have.text', 'Acceso correcto!');
+      cy.get('#volver').click();
+      /* ==== End Cypress Studio ==== */
+    })
+
+    
+  })
+})
